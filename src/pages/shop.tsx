519645832e60abe683de10d9c25b784398d59c9f
samandar <samandargofurov236@gmail.com>
2024-08-17 08:10:18 +0500

    update shop

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -22,44 +22,36 @@ const Shop = () => {
         <div className="container mx-auto max-w-[1050px]">
           <div className="flex flex-wrap gap-5 mb-36">
             {filteredCards.map((el, index) => (
-              <div
-                key={index}
-                className="bg-[#F9F8F8] w-[247px] p-7 rounded-3xl"
-              >
-                <span className="bg-[#274c5b] text-white px-4 py-2 text-xs rounded-lg">
-                  {el.category}
-                </span>
-                <div
-                  className="cursor-pointer"
-                  onClick={() => handleRedirect(el.id)}
-                >
-                  <img
-                    className="drop-shadow-4xl w-56 h-52"
-                    src={el.image}
-                    alt={el.title}
-                  />
-                </div>
-                <div className="flex flex-col gap-4">
-                  <h6 className="text-[#274C5B] font-bold text-lg">
-                    {el.title}
-                  </h6>
-                  <div className="flex items-center justify-between border-t-2 border-[#DEDDDD] pt-2">
-                    <div className="flex items-center gap-2">
-                      <p className="text-sm text-[#B8B8B8] line-through">
-                        {el.oldPrice}
-                      </p>
-                      <p className="text-md font-semibold text-[#274C5B]">
-                        {el.newPrice}
-                      </p>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      {Array.from({ length: el.rating }, (_, i) => (
-                        <img key={i} src={star} alt="" width={15} />
-                      ))}
-                    </div>
+              <div key={index} className="bg-[#F9F8F8] w-[247px] p-5 rounded-3xl">
+              <span className="bg-[#274c5b] text-white px-4 py-2 text-xs rounded-lg">
+                {el.category}
+              </span>
+              <div onClick={() => handleRedirect(el.id)}>
+                <img
+                  className="drop-shadow-4xl cursor-pointer w-[207px] h-56"
+                  src={el.image}
+                  alt={el.title}
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <h6 className="text-[#274C5B] font-bold text-md">{el.title}</h6>
+                <div className="flex items-center justify-between border-t-2 border-[#DEDDDD] pt-2">
+                  <div className="flex items-center gap-2">
+                    <p className="text-xs text-[#B8B8B8] line-through">
+                      {el.oldPrice}
+                    </p>
+                    <p className="text-sm font-semibold text-[#274C5B]">
+                      {el.newPrice}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    {Array.from({ length: el.rating }, (_, i) => (
+                      <img key={i} src={star} alt="" className="cursor-pointer" width={13} />
+                    ))}
                   </div>
                 </div>
               </div>
+            </div>
             ))}
           </div>
         </div>
